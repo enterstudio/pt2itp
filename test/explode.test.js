45d400e50237d2b5e36091517025af9ab7b55a48
ingalls <nicholas.ingalls@gmail.com>
2015-09-28 11:03:44 -0400

    Support MultiLineString to Multiple LineStrings

--- test/explode.test.js
+++ test/explode.test.js
@@ -61,6 +61,20 @@ test('explode', function(t) {
             }
         }]
     }).features[0].geometry.coordinates, [[-1,-1,], [0,0], [1,1],[2,2],[3,3]], '-1-> -3-> <-2- <-4-');
+    
+    var res = explode({
+        "type": "FeatureCollection",
+        "features": [{
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "MultiLineString",
+                "coordinates": [[[-1,-1],[0,0]],[[3,3], [2,2]],[[0,0],[1,1]], [[4,4], [3,3]]]
+            }
+        }]
+    });
+    t.deepEquals(res.features[0].geometry.coordinates, [ [ -1, -1 ], [ 0, 0 ], [ 1, 1 ] ]);
+    t.deepEquals(res.features[1].geometry.coordinates, [ [ 4, 4 ], [ 3, 3 ], [ 2, 2 ]]);
 
 t.end();
 });
