f4dcfa440cf74357e3809a006307a91012cfad49
ingalls <nicholas.ingalls@gmail.com>
2015-09-28 10:12:11 -0400

    Improve dedup fxn

--- test/explode.test.js
+++ test/explode.test.js
@@ -2,7 +2,6 @@ test = require('tape');
 explode = require('../lib/explode');
 
 test('explode', function(t) {
-    
     t.deepEquals(explode({
         "type": "FeatureCollection",
         "features": [{
@@ -13,9 +12,55 @@ test('explode', function(t) {
                 "coordinates": [[[0,0],[1,1]],[[0,0],[-1,-1]]]
             }
         }]
-    }).features[0].geometry.coordinates, [[-1,-1,], [0,0], [1,1]]);
+    }).features[0].geometry.coordinates, [[-1,-1,], [0,0], [1,1]], '<-- -->');
 
+    t.deepEquals(explode({
+        "type": "FeatureCollection",
+        "features": [{
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "MultiLineString",
+                "coordinates": [[[-1,-1],[0,0]],[[0,0],[1,1]]]
+            }
+        }]
+    }).features[0].geometry.coordinates, [[-1,-1,], [0,0], [1,1]], '--> -->');
+    
+    t.deepEquals(explode({
+        "type": "FeatureCollection",
+        "features": [{
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "MultiLineString",
+                "coordinates": [[[-1,-1],[0,0]],[[1,1],[0,0]]]
+            }
+        }]
+    }).features[0].geometry.coordinates, [[-1,-1,], [0,0], [1,1]], '--> <--');
+    
+    t.deepEquals(explode({
+        "type": "FeatureCollection",
+        "features": [{
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "MultiLineString",
+                "coordinates": [[[0,0],[-1,-1]],[[0,0],[1,1]]]
+            }
+        }]
+    }).features[0].geometry.coordinates, [[1,1,], [0,0], [-1,-1]], '<-- <--');
 
-    t.end();
+    t.deepEquals(explode({
+        "type": "FeatureCollection",
+        "features": [{
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "MultiLineString",
+                "coordinates": [[[-1,-1],[0,0]],[[2,2], [1,1]],[[0,0],[1,1]], [[3,3], [2,2]]]
+            }
+        }]
+    }).features[0].geometry.coordinates, [[-1,-1,], [0,0], [1,1],[2,2],[3,3]], '-1-> -3-> <-2- <-4-');
 
+t.end();
 });
