68291a69afb5f120e4b6df60051e045402ed15e9
ingalls <nicholas.ingalls@gmail.com>
2015-09-28 12:44:49 -0400

    add misc.det2d & misc.sign tests

---
+++ test/misc.test.js
@@ -0,0 +1,17 @@
+var misc = require('../lib/misc');
+var test = require('tape');
+
+test('misc.sign', function(assert) {
+    assert.equal(misc.sign(5), 1);
+    assert.equal(misc.sign(-5), -1);
+    assert.equal(misc.sign(0), 0);
+    assert.end();
+});
+
+test('misc.det2D', function(assert) {
+    assert.equal(misc.det2D([0,0], [1,2], [3,4]), -2);
+    assert.equal(misc.det2D([0,0], [2,1], [-1,3]), 7);
+    assert.equal(misc.det2D([1,1], [0,1], [2,3]), -2);
+    assert.equal(misc.det2D([2,2], [0,-1], [-3,1]), -13);
+    assert.end();
+});
