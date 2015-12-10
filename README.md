64d1c722141e454edb8ecd76b8945218922985de
ingalls <nicholas.ingalls@gmail.com>
2015-12-10 16:01:07 -0500

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# pt2itp
-Generate an interpolation network given a road network and address points
+# Pts => Interpolation
+
+given a road network and a set of address points in vector tile form, generate an interpolation network.
+
+## Input Data
+
+### Point Vector tiles
+
+Input geojson `FeatureCollection` file of points. Each point should have a property called `street` containing the street name
+and `number` containing the street address.
+
+tippecanoe -z14 -Z14 -l addresses -n Addresses -pf -pk -r 0 -o addresses.mbtiles addresses.geojson
+
+### Street Vector tiles
+
+Input geojson `FeatureCollection` file of lines. Each line should have a property called `street` containing the street name.
+
+tippecanoe -z14 -Z14 -y street -l streets -n Streets -ps -pf -pk -o streets.mbtiles streets.geojson
---
+++ test/interpolize.test.js
@@ -0,0 +1,37 @@
+var test = require('tape');
+var interpolize = require('../lib/interpolize');
+
+test('Interpolize', function(t) {
+    var street = {
+        type: "Feature",
+        properties: { street: "Battleridge Place" },
+        geometry: {
+            type: "LineString",
+            coordinates: [
+                [-77.21064805984497,39.1773849237293],
+                [-77.21062123775481,39.17687343078357]
+            ]
+        }
+    }
+
+    var address = {
+        type: "Feature",
+        properties: {
+            street: "Battleridge Place",
+            numbers: ["8","10","9","11"]
+        },
+        geometry: {
+            type: "MultiPoint",
+            coordinates: [
+                [-77.21054881811142,39.1769482836422],
+                [-77.21056759357452,39.17731007133552],
+                [-77.2107258439064,39.176966996844406],
+                [-77.21077680587769,39.177320467506085]
+            ]
+        }
+    }
+
+    var res = interpolize(street, address);
+
+    t.end();
+});
