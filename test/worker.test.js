ad30afbdb21e667e647ec9e25a81ced5b839a68b
ingalls <nicholas.ingalls@gmail.com>
2016-02-04 01:55:13 -0500

    Add worker fixture tests

--- test/worker.test.js
+++ test/worker.test.js
@@ -1,5 +1,8 @@
 var worker = require('../lib/worker');
 var test = require('tape');
+var path = require('path');
+var fs = require('fs');
+var _ = require('lodash');
 
 test('worker - no addresses', function(t) {
     worker({
@@ -32,3 +35,49 @@ test('worker - no streets', function(t) {
     });
 });
 
+test('worker - fixtures', function(t) {
+    var fixtures = fs.readdirSync(path.resolve(__dirname, '..', 'test/fixtures'));
+    fixtures.forEach(function(fixture) {
+        (function(fixture) {
+            t.test(function(q) {
+                var fixtures = require('./fixtures/' + fixture);
+
+                var streetFixture = fixtures.features.filter(function(fixture) {
+                    if (fixture.properties.address) return false;
+                    else if (fixture.geometry.type === 'Point') return false;
+                    else return true;
+                })[0];
+
+                var street = _.cloneDeep(streetFixture);
+                ['lstart', 'lend', 'rstart', 'rend'].forEach(function(key){
+                    delete street.properties[key]; 
+                });
+
+                var addresses = fixtures.features.filter(function(fixture) {
+                    if (fixture.properties.address) return true;
+                    else return false;
+                }).map(function(fixture) {
+                    if (!fixture.properties.street) fixture.properties.street = street.properties.street; 
+                    if (fixture.properties.address) fixture.properties.number = fixture.properties.address;
+                    return fixture;
+                });
+
+                worker({
+                    Addresses: {
+                        addresses: turf.featurecollection(addresses)
+                    },
+                    Streets: {
+                        streets: turf.featurecollection([street])
+                    }
+                }, [1,1,1], null, function(err, res) {
+                    t.equal(streetFixture.properties.lstart, res[0].properties.lstart, 'lstart matched');
+                    t.equal(streetFixture.properties.lend, res[0].properties.lend, 'lend matched');
+                    t.equal(streetFixture.properties.rstart, res[0].properties.rstart, 'rstart matched');
+                    t.equal(streetFixture.properties.rend, res[0].properties.rend, 'rend matched');
+                    q.end();    
+                });
+            });
+        })(fixture);
+    });
+    t.end();
+});
