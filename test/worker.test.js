584e52ed3b91a7b930998cd103fa4b71606104cd
ingalls <nicholas.ingalls@gmail.com>
2016-06-13 16:51:00 -0400

    Allow geom&feat tests,add new worker test

--- test/worker.test.js
+++ test/worker.test.js
@@ -8,11 +8,22 @@ var addresses = require('./fixtures/addresses.json');
 var streets = require('./fixtures/streets.json');
 
 test('worker - dump raw', function(t) {
+    global.mapOptions = {
+        raw: 'addresses'
+    };
+
+    var i = 0;
+
     worker({
         Addresses: { addresses: addresses },
         Streets: { streets: streets }
-    }, [3789,2373,12], null, function(err, res) {
-        t.equal(res.toString(), 'No street data in: 1,1,14');
+    }, [3789,2373,12], writeData, function(err, res) {
+        //t.deepEqual(res, addresses);
         t.end();    
     });
+
+    function writeData(data) {
+        t.deepEquals(data, addresses.features[i]);
+        i = i + 1;
+    }
 });
