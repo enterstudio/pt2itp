0d93e3fa341c57418309ee28c6407bbf479826b5
ingalls <nicholas.ingalls@gmail.com>
2016-06-14 11:05:04 -0400

    input output tests

--- test/convert.test.js
+++ test/convert.test.js
@@ -1,9 +1,27 @@
 var convert = require('../lib/convert');
 var test = require('tape');
 
-test('Feature', function(t) {
+test('Feature - no args', function(t) {
     convert(null, function(err) {
-        t.equals(err.toString(), 'Error: options object required')
+        t.equals(err.toString(), 'Error: options object required');
+        t.end();
+    });
+});
+
+test('Feature - no input', function(t) {
+    convert({
+        output: true
+    }, function(err) {
+        t.equals(err.toString(), 'Error: input path required');
+        t.end();
+    });
+});
+
+test('Feature - no output', function(t) {
+    convert({
+        input: true
+    }, function(err) {
+        t.equals(err.toString(), 'Error: output path required');
         t.end();
     });
 });
