be97b186ff2b25cb6ba9d27dea15c5b0d52dfb4c
ingalls <nicholas.ingalls@gmail.com>
2016-07-01 10:44:13 -0400

    Add simple cluster test

--- test/cluster.test.js
+++ test/cluster.test.js
@@ -1,5 +1,6 @@
 var test = require('tape');
-var clusterAddress = require('../lib/cluster');
+var cluster = require('../lib/cluster');
+var fs = require('fs');
 
 test('named clusters', function(t) {
     var feat  = {
@@ -25,7 +26,7 @@ test('named clusters', function(t) {
         }]
     }
 
-    var res = clusterAddress(feat);
+    var res = cluster(feat);
 
     t.equal(res.named.features.length, 1, 'produces 1 named feature');
     t.equal(res.unnamed.features.length, 0, 'produces 0 unnamed features');
@@ -72,7 +73,7 @@ test('cluster', function(t) {
         }]
     }
 
-    var res = clusterAddress(feat);
+    var res = cluster(feat);
 
     t.equal(res.named.features.length, 1, 'produces 1 named feature');
     t.equal(res.named.features[0].properties.street, 'Test Street');
@@ -82,4 +83,56 @@ test('cluster', function(t) {
     t.equal(res.unnamed.features[1].properties.street, null);
 
     t.end();
+});
+
+test('closestCluster', function(t) {
+    t.test('closestCluster - Straight Line', function(q) {
+        var res = cluster.closestCluster({
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "type": "LineString",
+                "coordinates": [ [ 152.9567563533783, -27.291505832532188 ], [ 152.95727133750913, -27.288550080213408 ] ] 
+            }
+        },{
+            "type": "FeatureCollection",
+            "features": [{
+                "type": "Feature",
+                "properties": {
+                    "street": "Main Street"
+                },
+                "geometry": {
+                    "type": "MultiPoint",
+                    "coordinates": [
+                        [ 152.9564130306244, -27.2910863111871 ],
+                        [ 152.95650959014893, -27.290428422250013 ],
+                        [ 152.9566490650177, -27.2897514599999 ],
+                        [ 152.95678853988645, -27.289036354549673 ],
+                    ]
+                }
+            },{
+                "type": "Feature",
+                "properties": {
+                    "street": "Other Avenue"
+                },
+                "geometry": {
+                    "type": "MultiPoint",
+                    "coordinates": [ 
+                        [ 152.95779705047605, -27.28927472354469 ],
+                        [ 152.95770049095154, -27.289579835111777 ],
+                        [ 152.95766830444336, -27.289884945840623 ]
+                    ]
+                }
+            }]
+        });
+
+        if (process.env.UPDATE) {
+            fs.writeFileSync(__dirname + '/fixtures/cluster.simple.json', JSON.stringify(res, null, 4));
+            t.fail('had to update fixture');
+        }
+
+        var fixture = require('./fixtures/cluster.simple.json');
+        q.deepEquals(res, fixture);
+        q.end();
+    });
 });
