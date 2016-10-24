6a5fe37428842bddca432a36d3b7fcb817fbdc04
ingalls <nicholas.ingalls@gmail.com>
2016-10-24 14:48:10 -0400

    Remove newly extraneous files

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var test = require('./lib/test');
 var map = require('./lib/map');
 var convert = require('./lib/convert');
 var settings = require('./package.json');
-var util = require('./lib/util');
 
 var argv = require('minimist')(process.argv, {
     boolean: ["help", "version"],
@@ -23,17 +22,6 @@ if (argv.help) {
 switch (argv._[2]) {
     case ("help"):
         help(argv);
-        break;
-    case ("util"):
-        util(process.argv, function(err, res) {
-            if (err) {
-                console.error(err.toString());
-                process.exit(1);
-            } else {
-                console.log(res);
-                process.exit(0);
-            }
-        });
         break;
     case ("map"):
         map(process.argv, function(err) {
