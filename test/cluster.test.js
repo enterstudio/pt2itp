1a92d3c7a4863c223e9f4e60d0282304bc6e589c
ingalls <nicholas.ingalls@gmail.com>
2016-11-01 11:44:34 -0400

    Test name module

--- test/cluster.test.js
+++ test/cluster.test.js
@@ -1,138 +1,114 @@
 var test = require('tape');
 var cluster = require('../lib/cluster');
 var fs = require('fs');
+var pg = require('pg');
+var Queue = require('d3-queue').queue;
 
-test('named clusters', function(t) {
-    var feat  = {
-        type: 'FeatureCollection',
-        features: [{
-            properties: {
-                street: 'Test Street',
-                number: 10
-            },
-            geometry: {
-                type: 'Point',
-                coordinates: [0,0]
-            }
-        },{
-            properties: {
-                street: 'Test Street',
-                number: '12'
-            },
-            geometry: {
-                type: 'Point',
-                coordinates: [1,1]
-            }
-        }]
-    }
-
-    var res = cluster(feat);
-
-    t.equal(res.named.features.length, 1, 'produces 1 named feature');
-    t.equal(res.unnamed.features.length, 0, 'produces 0 unnamed features');
-    t.deepEquals(res.named.features[0].properties.numbers, [ 10, 12 ], 'produces address cluster');
-    t.deepEquals(res.named.features[0].geometry.coordinates, [ [ 0, 0 ], [ 1, 1 ] ], 'produces multipoint');
-    t.end();
+var pool = new pg.Pool({
+    max: 10,
+    user: 'postgres',
+    database: 'pt_test',
+    idleTimeoutMillis: 30000
 });
 
-test('cluster', function(t) {
-    var feat  = {
-        type: 'FeatureCollection',
-        features: [{
-            properties: {
-                street: 'Test Street',
-                alternates: []
-            },
-            geometry: {
-                type: 'LineString'
-            }
-        },{
-            properties: {
-                street: 'Test Street',
-                alternates: []
-            },
-            geometry: {
-                type: 'LineString'
-            }
-        },{
-            properties: {
-                street: null,
-                alternates: []
-            },
-            geometry: {
-                type: 'LineString'
-            }
-        },{
-            properties: {
-                street: null,
-                alternates: []
-            },
-            geometry: {
-                type: 'LineString'
-            }
-        }]
-    }
+test('cluster.name', function(t) {
+    var popQ = Queue(1);
 
-    var res = cluster(feat);
-
-    t.equal(res.named.features.length, 1, 'produces 1 named feature');
-    t.equal(res.named.features[0].properties.street, 'Test Street');
-
-    t.equal(res.unnamed.features.length, 2, 'produces 2 unnamed features');
-    t.equal(res.unnamed.features[0].properties.street, null);
-    t.equal(res.unnamed.features[1].properties.street, null);
-
-    t.end();
-});
-
-test('closestCluster', function(t) {
-    t.test('closestCluster - Straight Line', function(q) {
-        var res = cluster.closestCluster({
-            "type": "Feature",
-            "properties": {},
-            "geometry": {
-                "type": "LineString",
-                "coordinates": [ [ 152.9567563533783, -27.291505832532188 ], [ 152.95727133750913, -27.288550080213408 ] ] 
-            }
-        },{
-            "type": "FeatureCollection",
-            "features": [{
-                "type": "Feature",
-                "properties": {
-                    "street": "Main Street"
-                },
-                "geometry": {
-                    "type": "MultiPoint",
-                    "coordinates": [
-                        [ 152.9564130306244, -27.2910863111871 ],
-                        [ 152.95650959014893, -27.290428422250013 ],
-                        [ 152.9566490650177, -27.2897514599999 ],
-                        [ 152.95678853988645, -27.289036354549673 ],
-                    ]
-                }
-            },{
-                "type": "Feature",
-                "properties": {
-                    "street": "Other Avenue"
-                },
-                "geometry": {
-                    "type": "MultiPoint",
-                    "coordinates": [ 
-                        [ 152.95779705047605, -27.28927472354469 ],
-                        [ 152.95770049095154, -27.289579835111777 ],
-                        [ 152.95766830444336, -27.289884945840623 ]
-                    ]
-                }
-            }]
+    //CREATE pt2itp TABLES
+    popQ.defer(function(done) {
+        pool.query(`
+            BEGIN;
+            CREATE TABLE address (id SERIAL, text TEXT, _text TEXT, number INT, geom GEOMETRY(POINT, 4326));
+            CREATE TABLE address_cluster (id SERIAL, text TEXT, _text TEXT, number TEXT, geom GEOMETRY(MULTIPOINT, 4326));
+            CREATE TABLE network (id SERIAL, text TEXT, _text TEXT, named BOOLEAN, geom GEOMETRY(LINESTRING, 4326));
+            CREATE TABLE network_cluster (id SERIAL, text TEXT, _text TEXT, address INT, geom GEOMETRY(MULTILINESTRING, 4326));
+            COMMIT;
+        `, function(err, res) {
+            t.error(err);
+            return done();
         });
-
-        if (process.env.UPDATE) {
-            fs.writeFileSync(__dirname + '/fixtures/cluster.simple.json', JSON.stringify(res, null, 4));
-            t.fail('had to update fixture');
-        }
-
-        var fixture = require('./fixtures/cluster.simple.json');
-        q.deepEquals(res, fixture);
-        q.end();
     });
+
+    //POPULATE NETWORK
+    popQ.defer(function(done) {
+        pool.query(`
+            BEGIN;
+            INSERT INTO network (id, geom) VALUES (1, ST_SetSRID(ST_GeomFromGeoJSON('{ "type": "LineString", "coordinates": [ [ -66.05180561542511, 45.26869136632906 ], [ -66.05007290840149, 45.268982070325656 ] ] }'), 4326));
+            COMMIT;
+        `, function(err, res) {
+            t.error(err);
+            return done();
+        });
+    });
+
+    //POPULATE ADDRESS
+    popQ.defer(function(done) {
+        pool.query(`
+            BEGIN;
+            INSERT INTO address (id, text, _text, number, geom) VALUES (1, 'main st', 'Main Street', 10, ST_SetSRID(ST_GeomFromGeoJSON('{ "type": "Point", "coordinates": [ -66.05154812335967, 45.26861208316249 ] }'), 4326));
+            INSERT INTO address (id, text, _text, number, geom) VALUES (2, 'main st', 'Main Street', 10, ST_SetSRID(ST_GeomFromGeoJSON('{ "type": "Point", "coordinates": [ -66.05125308036804, 45.26868759094269 ] }'), 4326));
+            INSERT INTO address (id, text, _text, number, geom) VALUES (3, 'main st', 'Main Street', 10, ST_SetSRID(ST_GeomFromGeoJSON('{ "type": "Point", "coordinates": [ -66.05092048645020, 45.26872912017898 ] }'), 4326));
+            INSERT INTO address (id, text, _text, number, geom) VALUES (4, 'main st', 'Main Street', 10, ST_SetSRID(ST_GeomFromGeoJSON('{ "type": "Point", "coordinates": [ -66.05050742626190, 45.26880462780347 ] }'), 4326));
+            COMMIT;
+        `, function(err, res) {
+            t.error(err);
+            return done();
+        });
+    });
+
+    popQ.defer(function(done) {
+        cluster.name(1, pool, function(err) {
+            t.error(err);
+            return done();
+        });
+    });
+
+    popQ.defer(function(done) {
+        pool.query(`
+            SELECT id, _text, text, named FROM network;
+        `, function(err, res) {
+            t.error(err);
+
+            t.deepEquals(res.rows[0], {
+                id: 1,
+                _text: 'Main Street',
+                text: 'main st',
+                named: true
+            });
+            return done();
+        });
+    });
+
+    popQ.await(function(err) {
+        t.error(err);
+
+        pool.query(`
+            BEGIN;
+            DROP TABLE address;
+            DROP TABLE address_cluster;
+            DROP TABLE network;
+            DROP TABLE network_cluster;
+            COMMIT;
+        `, function(err, res) {
+            t.error(err);
+            t.end();
+        });
+    });
+});
+
+test('cluster.match', function(t) {
+    t.end();
+});
+
+test('cluster.address', function(t) {
+    t.end();
+});
+
+test('cluster.network', function(t) {
+    t.end();
+});
+
+test('end connection', function(t) {
+    pool.end();
+    t.end();
 });
