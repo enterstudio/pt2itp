7011a6ef96f329a1205f3ef00d60f9ca453b936b
ingalls <nicholas.ingalls@gmail.com>
2016-11-01 16:19:58 -0400

    Run through sg address layer

--- test/map.test.js
+++ test/map.test.js
@@ -2,6 +2,7 @@ var worker = require('../lib/map');
 var test = require('tape');
 var path = require('path');
 var fs = require('fs');
+var pg = require('pg');
 
 test('map - in-address error', function(t) {
     worker({
@@ -48,7 +49,28 @@ test('map - output error', function(t) {
         'output': '/tmp/itp.geojson',
         'db': 'pt_test'
     }, function(err, res) {
-        t.equals(err.toString(), 'Error: --db=<DATABASE> argument required');
+        t.error(err);
+        t.end();
+    });
+});
+
+test('drop database', function(t) {
+    var pool = new pg.Pool({
+        max: 10,
+        user: 'postgres',
+        database: 'pt_test',
+        idleTimeoutMillis: 30000
+    });
+
+    pool.query(`
+        BEGIN;
+        DROP TABLE address;
+        DROP TABLE address_cluster;
+        DROP TABLE network;
+        DROP TABLE network_cluster;
+        COMMIT;
+    `, function(err) {
+        t.error(err);
         t.end();
     });
 });
