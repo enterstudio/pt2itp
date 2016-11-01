b208cc7a429d109020526a767b3d0845ca33397b
ingalls <nicholas.ingalls@gmail.com>
2016-11-01 16:01:39 -0400

    _text => street in fixture

--- test/map.test.js
+++ test/map.test.js
@@ -3,7 +3,7 @@ var test = require('tape');
 var path = require('path');
 var fs = require('fs');
 
-test('map', function(t) {
+test('map - in-address error', function(t) {
     worker({
     }, function(err, res) {
         t.equals(err.toString(), 'Error: --in-address=<FILE.geojson> argument required');
@@ -11,12 +11,44 @@ test('map', function(t) {
     });
 });
 
-test('map', function(t) {
+test('map - in-network error', function(t) {
     worker({
         'in-address': './test/fixtures/sg-address.geojson'
     }, function(err, res) {
-        t.equals(err.toString(), 'Error: --in-address=<FILE.geojson> argument required');
+        t.equals(err.toString(), 'Error: --in-network=<FILE.geojson> argument required');
         t.end();
     });
 });
 
+test('map - output error', function(t) {
+    worker({
+        'in-address': './test/fixtures/sg-address.geojson',
+        'in-network': './test/fixtures/sg-network.geojson'
+    }, function(err, res) {
+        t.equals(err.toString(), 'Error: --output=<FILE.geojson> argument required');
+        t.end();
+    });
+});
+
+test('map - db error', function(t) {
+    worker({
+        'in-address': './test/fixtures/sg-address.geojson',
+        'in-network': './test/fixtures/sg-network.geojson',
+        'output': '/tmp/itp.geojson'
+    }, function(err, res) {
+        t.equals(err.toString(), 'Error: --db=<DATABASE> argument required');
+        t.end();
+    });
+});
+
+test('map - output error', function(t) {
+    worker({
+        'in-address': './test/fixtures/sg-address.geojson',
+        'in-network': './test/fixtures/sg-network.geojson',
+        'output': '/tmp/itp.geojson',
+        'db': 'pt_test'
+    }, function(err, res) {
+        t.equals(err.toString(), 'Error: --db=<DATABASE> argument required');
+        t.end();
+    });
+});
