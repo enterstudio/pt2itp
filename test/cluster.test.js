21eb2839e15eb32cfd6d4569870042f1500323cb
ingalls <nicholas.ingalls@gmail.com>
2016-11-01 14:30:44 -0400

    Add cluster.network test

--- test/cluster.test.js
+++ test/cluster.test.js
@@ -246,7 +246,69 @@ test('cluster.address', function(t) {
 });
 
 test('cluster.network', function(t) {
-    t.end();
+    var popQ = Queue(1);
+
+    //CREATE pt2itp TABLES
+    popQ.defer(function(done) {
+        pool.query(`
+            BEGIN;
+            CREATE TABLE network (id SERIAL, text TEXT, _text TEXT, geom GEOMETRY(LINESTRING, 4326));
+            CREATE TABLE network_cluster (id SERIAL, text TEXT, _text TEXT, geom GEOMETRY(MULTILINESTRING, 4326));
+            COMMIT;
+        `, function(err, res) {
+            t.error(err);
+            return done();
+        });
+    });
+
+    //POPULATE NETWORK
+    popQ.defer(function(done) {
+        pool.query(`
+            BEGIN;
+            INSERT INTO network (id, text, _text, geom) VALUES (1, 'main st', 'Main Street', ST_SetSRID(ST_GeomFromGeoJSON('{ "type": "LineString", "coordinates": [ [ -66.05390310287476, 45.26961632842303 ], [ -66.05441808700562, 45.271035832768376 ] ]}'), 4326));
+            INSERT INTO network (id, text, _text, geom) VALUES (2, 'main st', 'Main Street', ST_SetSRID(ST_GeomFromGeoJSON('{ "type": "LineString", "coordinates": [ [ -66.05435371398926, 45.27100563091792 ], [ -66.05493307113646, 45.27245530161207 ] ]}'), 4326));
+            INSERT INTO network (id, text, _text, geom) VALUES (3, 'main st', 'Main Street', ST_SetSRID(ST_GeomFromGeoJSON('{ "type": "LineString", "coordinates": [ [ -113.50117206573485, 53.55137413785917 ], [ -113.50112915039062, 53.54836549323335 ] ]}'), 4326));
+            INSERT INTO network (id, text, _text, geom) VALUES (4, 'main st', 'Main Street', ST_SetSRID(ST_GeomFromGeoJSON('{ "type": "LineString", "coordinates": [ [ -113.50100040435791, 53.54836549323335 ], [ -113.50104331970215, 53.54614711825744 ] ]}'), 4326));
+            COMMIT;
+        `, function(err, res) {
+            t.error(err);
+            return done();
+        });
+    });
+
+    popQ.defer(function(done) {
+        cluster.network('main st', pool, function(err) {
+            t.error(err);
+            return done();
+        });
+    });
+
+    popQ.defer(function(done) {
+        pool.query(`
+            SELECT id, text, ST_AsGeoJSON(geom) AS geom FROM network_cluster;
+        `, function(err, res) {
+            t.error(err);
+
+            t.equals(res.rows.length, 2);
+            t.deepEquals(res.rows[0], { geom: '{"type":"MultiLineString","coordinates":[[[-66.0539031028748,45.269616328423],[-66.0544180870056,45.2710358327684]],[[-66.0543537139893,45.2710056309179],[-66.0549330711365,45.2724553016121]]]}', id: 1, text: 'main st' });
+            t.deepEquals(res.rows[1], { geom: '{"type":"MultiLineString","coordinates":[[[-113.501172065735,53.5513741378592],[-113.501129150391,53.5483654932333]],[[-113.501000404358,53.5483654932333],[-113.501043319702,53.5461471182574]]]}', id: 2, text: 'main st' });
+            return done();
+        });
+    });
+
+    popQ.await(function(err) {
+        t.error(err);
+
+        pool.query(`
+            BEGIN;
+            DROP TABLE network;
+            DROP TABLE network_cluster;
+            COMMIT;
+        `, function(err, res) {
+            t.error(err);
+            t.end();
+        });
+    });
 });
 
 test('end connection', function(t) {
