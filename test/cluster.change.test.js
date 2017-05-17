1f9eddae4ed9ed8866b34470195b4cdb536e5476
aarthykc <aarthy@mapbox.com>
2017-05-16 17:57:24 -0700

    modularize it

--- test/cluster.change.test.js
+++ test/cluster.change.test.js
@@ -1,9 +1,10 @@
 const Cluster = require('../lib/cluster');
-
-const test = require('tape');
 const fs = require('fs');
+const test = require('tape');
 const pg = require('pg');
 const Queue = require('d3-queue').queue;
+
+const jsonFixture = require(__dirname + '/fixtures/cluster.address.json');
 
 const pool = new pg.Pool({
     max: 10,
@@ -14,7 +15,12 @@ const pool = new pg.Pool({
 
 const cluster = new Cluster(pool);
 
-test('Populate and cluster address points', (t) => {
+/*
+- [ ] function to read all geojsons 
+- [ ] function to run each with ST_AsGeoJSON
+- [ ]
+*/
+function createClusteredGeojsons(jsonFixture){
     const popQ = Queue(1);
 
     //CREATE pt2itp TABLES
@@ -31,33 +37,36 @@ test('Populate and cluster address points', (t) => {
             CREATE TABLE network_cluster (id SERIAL, text TEXT, _text TEXT, address INT, geom GEOMETRY(MULTILINESTRING, 4326), buffer GEOMETRY(POLYGON, 4326));
             COMMIT;
         `, (err, res) => {
-            t.error(err);
+            if (err) console.log(err);
             return done();
         });
     });
 
     //POPULATE ADDRESS
-    popQ.defer((done) => {
-        pool.query(`
-            BEGIN;
-            INSERT INTO address (id, text, _text, number, geom) VALUES (1, 'main st', 'Main Street', 10, ST_SetSRID(ST_GeomFromGeoJSON('{ "type": "Point","coordinates": [ 9.51413869857788,47.132724392963944 ] }'), 4326));
-            INSERT INTO address (id, text, _text, number, geom) VALUES (2, 'main st', 'Main Street', 10, ST_SetSRID(ST_GeomFromGeoJSON('{ "type": "Point","coordinates": [ 9.516541957855225,47.132724392963944 ] }'), 4326));
-            COMMIT;
-        `, (err, res) => {
-            t.error(err);
-            return done();
-        });
+    jsonFixture.features.forEach(function(i) {
+        popQ.defer((done) => {
+            pool.query(`
+                BEGIN;
+                INSERT INTO address (id, text, _text, number, geom) VALUES (1, 'main st', 'Main Street', 10, ST_SetSRID(ST_GeomFromGeoJSON(i[0]), 4326));
+                INSERT INTO address (id, text, _text, number, geom) VALUES (2, 'main st', 'Main Street', 10, ST_SetSRID(ST_GeomFromGeoJSON(i[1]), 4326));
+                COMMIT;
+            `, (err, res) => {
+                if (err) console.log(err);
+                return done();
+                });
+            });
+        };
     });
 
     popQ.defer((done) => {
         cluster.address((err) => {
-            t.error(err);
+            if (err) console.log(err);
             return done();
         });
     });
 
     popQ.await((err) => {
-        t.error(err);
+        if (err) console.log(err);
 
         pool.query(`
             BEGIN;
@@ -65,8 +74,20 @@ test('Populate and cluster address points', (t) => {
             DROP TABLE network_cluster;
             COMMIT;
         `, (err, res) => {
-            t.error(err);
-            t.end();
+            if (err) console.log(err);
         });
     });
+});
+}
+
+
+function createClusteredGeojsons(jsonFixture) {
+    jsonFixture.features.forEach(function(i) {
+            console.log(i[0], i[1]);
+    });
+}
+
+test('end connection', (t) => {
+    pool.end();
+    t.end();
 });
