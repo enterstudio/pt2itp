3df6863436caa0371b5932775e51896f227a6d45
ingalls <nick@mapbox.com>
2017-06-21 20:15:20 +0000

    And another one falls

--- test/interpolize.test.js
+++ test/interpolize.test.js
@@ -278,40 +278,36 @@ test('Interpolize - Addr past line end - opposite', (t) => {
 });
 
 test('Interpolize - Addr past line end - bend', (t) => {
-    let street = {
-        type: "Feature",
-        properties: {
-            street: ["Battleridge", "Place"],
-            'carmen:text': 'Battleridge Place'
+    let segs = [{
+        network: {
+            type: "Feature",
+            properties: { },
+            geometry: {
+                type: "LineString",
+                coordinates: [
+                    [ -77.21002042293549, 39.17696283835544 ],
+                    [ -77.20934987068176, 39.17688382701869 ],
+                    [ -77.20870077610016, 39.177050166571725 ]
+                ]
+            }
         },
-        geometry: {
-            type: "LineString",
-            coordinates: [
-                [ -77.21002042293549, 39.17696283835544 ],
-                [ -77.20934987068176, 39.17688382701869 ],
-                [ -77.20870077610016, 39.177050166571725 ]
-            ]
-        }
-    }
-
-    let address = {
-        type: "Feature",
-        properties: {
-            street: ["Battleridge", "Place"],
-            numbers: [ "2", "4", "1", "3"]
+        address: {
+            type: "Feature",
+            properties: { },
+            geometry: {
+                type: "MultiPoint",
+                coordinates: [
+                    [ -77.20983803272247, 39.17702937414912 ],
+                    [ -77.20847547054291, 39.177740471511456 ],
+                    [ -77.20990777015686, 39.17674659659119 ],
+                    [ -77.20825552940369, 39.1777238377372 ]
+                ]
+            }
         },
-        geometry: {
-            type: "MultiPoint",
-            coordinates: [
-                [ -77.20983803272247, 39.17702937414912 ],
-                [ -77.20847547054291, 39.177740471511456 ],
-                [ -77.20990777015686, 39.17674659659119 ],
-                [ -77.20825552940369, 39.1777238377372 ]
-            ]
-        }
-    }
+        number: [ "2", "4", "1", "3"]
+    }];
 
-    let res = interpolize(street, address, { debug: true });
+    let res = interpolize('Battleridge Place', segs, { debug: true });
 
     if (process.env.UPDATE) {
         fs.writeFileSync(__dirname + '/fixtures/itp-pastline-bend.json', JSON.stringify(res, null, 4));
