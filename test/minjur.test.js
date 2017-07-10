245cf4a80733f6e874a10cf5a52be293ac137c85
ingalls <nick@mapbox.com>
2017-07-10 08:31:20 -0400

    Add minjur tests

---
+++ test/minjur.test.js
@@ -0,0 +1,68 @@
+const map = require('../lib/map/minjur').map;
+const test = require('tape');
+
+test('Minjur', (t) => {
+    t.equals(map({
+        type: 'Feature',
+        geometry: {
+            type: 'Polygon'
+        }
+    }), false, 'Feat must be a (Multi)LineString geom');
+
+    t.equals(map({
+        type: 'Feature',
+        geometry: {
+            type: 'LineString'
+        }
+    }), false, 'Feat must have Properties');
+
+    t.equals(map({
+        type: 'Feature',
+        properties: { },
+        geometry: {
+            type: 'LineString'
+        }
+    }), false, 'Feat must have Highway');
+
+    //Streets allowed to be missing names
+    for (let type of ['motorway', 'trunk', 'primary', 'secondary', 'tertiary', 'residential', 'unclassified', 'living_street', 'pedestrian', 'road']) {
+        t.deepEquals(map({
+            type: 'Feature',
+            properties: {
+                highway: type
+            },
+            geometry: {
+                type: 'LineString'
+            }
+        }), { geometry: { type: 'LineString' }, properties: { street: '' }, type: 'Feature' }, `${type} is accepted`);
+    }
+
+    //Streets required to have names as they are lower quality tags
+    for (let type of ['track', 'service', 'construction', 'proposed', 'footway']) {
+        t.equals(map({
+            type: 'Feature',
+            properties: {
+                highway: type
+            },
+            geometry: {
+                type: 'LineString'
+            }
+        }), false, `${type} requires name`);
+    }
+
+    for (let type of ['track', 'service', 'construction', 'proposed', 'footway']) {
+        t.deepEquals(map({
+            type: 'Feature',
+            properties: {
+                highway: type,
+                name: 'Test'
+            },
+            geometry: {
+                type: 'LineString'
+            }
+        }), { geometry: { type: 'LineString' }, properties: { street: 'Test' }, type: 'Feature' }, `${type} is accepted w/ name`);
+    }
+
+    t.end();
+});
+
