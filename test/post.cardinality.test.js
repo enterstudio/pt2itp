f8f9e9b8e5ca411b6ae08774f932f79bf9b8183c
ingalls <nick@mapbox.com>
2017-07-10 11:42:44 -0400

    Tests for all states of false

--- test/post.cardinality.test.js
+++ test/post.cardinality.test.js
@@ -2,6 +2,14 @@ const post = require('../lib/post/cardinality').post;
 const test = require('tape');
 
 test('Post: Cardinality', (t) => {
+    t.equals(post(), undefined);
+    t.deepEquals(post({}), {});
+    t.deepEquals(post({
+        properties: {}
+    }), {
+        properties: {}
+    });
+
     t.equals(post({
         type: 'Feature',
         properties: {
@@ -29,6 +37,13 @@ test('Post: Cardinality', (t) => {
             'carmen:text': 'South Main St'
         }
     }).properties['carmen:text'], 'South Main St,Main St South');
+
+    t.equals(post({
+        type: 'Feature',
+        properties: {
+            'carmen:text': 'Main St South'
+        }
+    }).properties['carmen:text'], 'Main St South,South Main St');
 
     //At the moment we don't try to see if the synonyms are the same just that if there is a prefix/postfix existing already to skip
     t.equals(post({
